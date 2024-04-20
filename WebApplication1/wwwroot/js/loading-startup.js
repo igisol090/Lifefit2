897599445482a83f841bc8c8c7da5dde53b261df
Mitch <earlmitchellepelegre@gmail.com>
2024-04-20 16:53:57 +0800

    Add project files.

---
+++ WebApplication1/wwwroot/js/loading-startup.js
@@ -0,0 +1,18 @@
+﻿let progressBar = document.getElementById('progress');
+let width = 1;
+let interval = setInterval(increaseProgress, 20);
+
+function increaseProgress() {
+    alert('test')
+    if (width >= 100) {
+        clearInterval(interval);
+        setTimeout(() => {
+            window.location.href = 'forum.html';
+        }, 1000);
+    } else {
+        width++;
+        progressBar.style.width = width + '%';
+    }
+}
+
+
---
+++ WebApplication1/wwwroot/js/workout-page.js
@@ -0,0 +1,15 @@
+﻿let progressBar = document.getElementById('progress');
+let width = 1;
+let interval = setInterval(increaseProgress, 20);
+
+function increaseProgress() {
+    if (width >= 100) {
+        clearInterval(interval);
+        setTimeout(() => {
+            window.location.href = 'workoutpage.html';
+        }, 1000);
+    } else {
+        width++;
+        progressBar.style.width = width + '%';
+    }
+}
